816eb159f72455933cb9336f56e8351cb26ec903
anitaprova <104943181+anitaprova@users.noreply.github.com>
2024-01-06 22:53:25 -0500

    added navbar for testing

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,9 @@
 import React from "react";
+import Navbar from "./Navbar.tsx";
 
 export default function Home() {
   return <>
+    <Navbar/>
     <div className = "font-link">
       <h1 id="title">Prisma Skies</h1>
       <p>Press to Start</p>
