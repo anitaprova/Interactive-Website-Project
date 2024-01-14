28993d07335bc087298f0ed7483e9e4048b3ed0b
anitaprova <104943181+anitaprova@users.noreply.github.com>
2024-01-13 21:17:29 -0500

    made image map work

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,8 +6,49 @@ export default function Home() {
     <>
       <Navbar />
       <div>
-        <h1 id="title">Prisma Skies</h1>
-        <p>Press to Start</p>
+        <h1>Prisma Skies</h1>
+        {/* <img id="homepage-image" src="./src/assets/pole1.png"></img> */}
+        <img
+          src="./src/assets/pole1.png"
+          id="homepage-image"
+          useMap="#image-map"
+        ></img>
+
+        <map name="image-map">
+          <area
+            target=""
+            alt="Red"
+            title="Red"
+            href=""
+            coords="110,489,2,484,0,0,1918,0,1918,63,305,495"
+            shape="poly"
+          />
+          <area
+            target=""
+            alt="Pink"
+            title="Pink"
+            href=""
+            coords="0,486,300,496,1917,64,1918,492,1420,443,-2,981"
+            shape="poly"
+          />
+          <area
+            target=""
+            alt="Grey"
+            title="Grey"
+            href=""
+            coords="0,981,-2,1076,207,1076,417,962,785,866,1129,801,1208,711,1918,811,1917,496,1425,444,78,954,42,966"
+            shape="poly"
+          />
+          <area
+            target=""
+            alt="Yellow"
+            title="Yellow"
+            href=""
+            coords="208,1076,412,963,934,835,1132,799,1207,714,1915,817,1917,1076"
+            shape="poly"
+          />
+        </map>
+        <p>Press Any of the Fragments to Start</p>
       </div>
     </>
   );
