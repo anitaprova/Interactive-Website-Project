2edce087ab81ef590433b88802bcb7349f5dd0aa
anitaprova <104943181+anitaprova@users.noreply.github.com>
2024-01-13 19:56:10 -0500

    finished base for character page

--- src/components/Characters.tsx
+++ src/components/Characters.tsx
@@ -1,60 +1,27 @@
 import React from "react";
 import Navbar from "./Navbar";
+import data from "../data.json";
+// console.log(data);
 
-export default function Characters() 
-{
-	return (
+export default function Characters() {
+  const character_sheet = data.characters;
+  
+  return (
     <>
       <Navbar></Navbar>
       <h1>Characters</h1>
-      <div className="characterblock">
-        <div>
-          <p>Amari Inaya</p>
-          <p>
-            Personality Traits: People-pleaser, Complacent/fickle, Observant,
-            Shy
-          </p>
-          <p>
-            Occupation: Dropped out of college after her freshman year for her
-            brother to go to college
-          </p>
-          <p>Fun Fact: Journals to cope</p>
-        </div>
-
-        <div>
-          <p>Benoni Inaya</p>
-          <p>Personality Traits: Bad temper</p>
-          <p>Occupation: College Student</p>
-          <p>Fun Fact: His friend died</p>
-        </div>
-
-        <div>
-          <p>Amari's Father</p>
-          <p>Personality Traits: Absent minded and horrible communicator</p>
-          <p>Occupation: Failing Business Owner</p>
-          <p>Fun Fact: He has no fun facts!</p>
-        </div>
-
-        <div>
-          <img src="./src/assets/vincent.png" width="500" height="600"></img>
-          <p>Vincent Legend</p>
-          <p>
-            Personality Traits: Manipulative, gaslight gatekeep, but no
-            girlboss, narcissistic
-          </p>
-          <p>Occupation: Job in his family's business</p>
-          <p>
-            Fun Fact: His real name is Mohammad but he changed it to Vincent
-            Legend
-          </p>
-        </div>
-
-        <div>
-          <p>Aurora</p>
-          <p>Personality Traits: Well meaning</p>
-          <p>Occupation: Good Student</p>
-          <p>Fun Fact: Very Broke</p>
-        </div>
+      <div className="character-container">
+        {character_sheet.map((item) => (
+          <div className="character-item">
+            <img src={item.img} width="500" height="500"></img>
+            <div className="character-text">
+              <h2>{item.Name}</h2>
+              <p>Personality Traits: {item["Personality Traits"]}</p>
+              <p>Occupation: {item.Occupation}</p>
+              <p>Fun Fact: {item["Fun Fact"]}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </>
   );
