5d17bb4606feef7e335da7ddb1e0d9fa6fa32c15
Michelle <michelleh11219@gmail.com>
2024-01-20 02:06:41 -0500

    add icons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,24 @@
 import React from "react";
 import { NavLink, Outlet } from "react-router-dom";
+import "./navbar.css";
 
 export default function Navbar() { 
   return (
   <>
-    <nav className = "nav">
-    <NavLink to="/">Home</NavLink>
+    <nav>
     <ul>
+      {/* home.png is smaller and has more padding around the icon */}
       <li>
-        <NavLink to="/characters">Characters</NavLink>
+        <NavLink to="/"><img src= "src\assets\icons\home.png" /></NavLink>
       </li>
       <li>
-        <NavLink to="/stats">Statistics</NavLink>
+        <NavLink to="/characters"><img src= "src\assets\icons\characters.png" /></NavLink>
       </li>
       <li>
-        <NavLink to="/contact">Contact</NavLink>
+        <NavLink to="/stats"><img src= "src\assets\icons\stats.png" /></NavLink>
+      </li>
+      <li>
+        <NavLink to="/contact"><img src= "src\assets\icons\contact.png" /></NavLink>
       </li>
     </ul>
     </nav>
